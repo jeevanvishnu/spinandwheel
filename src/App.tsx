9a9479abfad4bbfb05e09af4d4e19ba6c3579216
jeevanvishnu <jeevanvishnu18@gmail.com>
2026-08-12 15:01:30 +0530

    feat: initialize project structure with React, Vite, and custom assets

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import SpinWheel from './components/SpinWheel';
+
+function App() {
+  return (
+    <main className="w-full min-h-screen bg-[#0d0b09]">
+      <SpinWheel />
+    </main>
+  );
+}
+
+export default App;
